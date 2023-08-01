52c9596736b9c21eb1c0092935eb138e79c48827
kernelk14 <khyleisaias@gmail.com>
2023-08-01 11:21:10 +0800

    Added documentation

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Import needed stuff
 import { Octokit, App } from "https://esm.sh/octokit?dts"
 import { exists } from "https://deno.land/std/fs/mod.ts"
-
+import username from "https://deno.land/x/username/mod.ts"
+// const uname = await exec("whoami")
+// console.log(uname)
 // Get the auth file
 const auth_file = await Deno.readTextFile("./gh_personal_access.txt")
-
+const uname = await username()
 const octokit = new Octokit ({
   auth: auth_file
 })
@@ -15,8 +17,13 @@ const {
 } = await octokit.rest.users.getAuthenticated();
 console.log(`Hello, ${login}!`)
 
+await octokit.request('GET /user', {
+    headers: {
+        'X-Github-Api-version': '2022-11-28'
+    }
+})
 // Getting the list of repo subs
-const subfile = "/home/khyle/.reposub"
+const subfile = `/home/${uname}/.reposub`
 
 try { 
   const parseFile = await Deno.readTextFile(subfile)
@@ -24,7 +31,7 @@ try {
   if (splitNames.includes("\n")) {
     // splitNames.replace("\n", " ")
     // plsParse(splitNames)
-    splitNames = file.split("\n")
+    splitNames = parseFile.split("\n")
   }
   // console.log(file)
   
@@ -35,27 +42,35 @@ try {
     i++
   }
   const input = prompt("> ")
-  console.log(splitNames[input-1])
-  let keyword = splitNames[input-1]
-  let repos = keyword.split("/")
+  // console.log(splitNames[input-1])
+  const keyword = splitNames[input-1]
+  const kw_newline = keyword.replace("\n", "")
+  const repos = kw_newline.split("/")
+  
+  if (repos[1].endsWith("\n")) {
+    repos[1] = repos[1].replace("\n", " ")
+  }
+  // console.log(repos)
   const printrepo = await octokit.request(`GET /repos/{owner}/{repo}/commits`, {
       owner: repos[0],
-      repo: repos[1],
+      repo: repos[1].replace("\n", ""),
       headers: {
           'X-Github-Api-version': '2022-11-28'
       }
   })
-  var data = printrepo['data']
+  // console.log(printrepo)
+  const data = printrepo['data']
   let j = 0
   console.log("Commit Lists:")
   while (j < data.length) {
-      var d1 = data[j]['commit']['message']
-      var d2 = data[j]['commit']['committer']['name']
-      var d3 = data[j]['commit']['committer']['date']
+      const d1 = data[j]['commit']['message']
+      const d2 = data[j]['commit']['committer']['name']
+      const d3 = data[j]['commit']['committer']['date']
       console.log(`\nDate: ${d3}\nCommitter: ${d2}\nCommit: ${d1}\n------------------------------------------------------`)
       j++
   }
   console.log("\n\n")
+  Deno.exit(0)
 } catch(error) {
   if (!(error instanceof Deno.errors.NotFound)) {
     console.log("You don't have a `.reposub` file in your home directory. Please create one first.")
